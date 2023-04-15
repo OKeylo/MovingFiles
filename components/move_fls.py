77f7a7e961b0a2af55961ba2770615e8c391d3ec
OKeylo <zabaev228@gmail.com>
2023-04-15 23:48:20 +0500

    styles(move_fls): swap origin and destination paths

--- components/move_fls.py
+++ components/move_fls.py
@@ -6,33 +6,33 @@ class Move_fls(CTkFrame):
     def __init__(self, master):
         super().__init__(master=master)
         
-        self.label_folder_path = CTkLabel(self,
-                                          text="The path for the folder")
-        self.label_folder_path.pack(anchor=W, padx=50, pady=(15,0))
+        self.label_origin_path = CTkLabel(self,
+                                         text="Origin path")
+        self.label_origin_path.pack(anchor=W, padx=50, pady=(0,0))
 
-        self.entry_folder_path = CTkEntry(self,
-                                          placeholder_text="Enter the path for the folder using \\\\", 
-                                          width=500)
-        self.entry_folder_path.pack(anchor=W, padx=50, pady=(0,5))
-
-        self.button_fold_sel = CTkButton(self,
-                           text="Folder Selection",
-                           command=lambda : self.open_folder_selection(self.entry_folder_path))
-        self.button_fold_sel.pack(anchor=E, padx=50)
-
-        self.label_files_path = CTkLabel(self,
-                                         text="The path from where the files will be moved")
-        self.label_files_path.pack(anchor=W, padx=50, pady=(0,0))
-
-        self.entry_files_path = CTkEntry(self, 
+        self.entry_origin_path = CTkEntry(self, 
                                           placeholder_text="Enter the path from where the files will be moved using \\\\", 
                                           width=500)
-        self.entry_files_path.pack(anchor=W, padx=50, pady=(0,5))
+        self.entry_origin_path.pack(anchor=W, padx=50, pady=(0,5))
 
         self.button_path_sel = CTkButton(self,
                            text="Folder Selection",
-                           command=lambda: self.open_folder_selection(self.entry_files_path))
+                           command=lambda: self.open_folder_selection(self.entry_origin_path))
         self.button_path_sel.pack(anchor=E, padx=50)
+
+        self.label_destination_path = CTkLabel(self,
+                                          text="Destination path")
+        self.label_destination_path.pack(anchor=W, padx=50, pady=(15,0))
+
+        self.entry_destination_path = CTkEntry(self,
+                                          placeholder_text="Enter the path where the files will be moved to using \\\\", 
+                                          width=500)
+        self.entry_destination_path.pack(anchor=W, padx=50, pady=(0,5))
+
+        self.button_fold_sel = CTkButton(self,
+                           text="Folder Selection",
+                           command=lambda : self.open_folder_selection(self.entry_destination_path))
+        self.button_fold_sel.pack(anchor=E, padx=50)
 
         self.label_files_extension = CTkLabel(self,
                                               text="Files extension")
@@ -65,10 +65,10 @@ class Move_fls(CTkFrame):
         set_appearance_mode(choice)
 
     def move_files_same_ext(self):
-        folder_path = self.entry_folder_path.get()
-        files_path = self.entry_files_path.get()
+        origin_path = self.entry_origin_path.get()
+        destination_path = self.entry_destination_path.get()
         files_extension = self.entry_files_extension.get()
-        move_files_same_extension(folder_path, files_path, files_extension)
+        move_files_same_extension(destination_path, origin_path, files_extension)
 
     def open_folder_selection(self, entry_id):
         folder_path = filedialog.askdirectory()
